2c1f680e0cdce3b208793720b738bfddda50a8e3
sharmiladutta <sharmilatech2016@gmail.com>
2017-09-01 11:47:18 +0530

    Add a new project..
    Add a new project..

---
+++ MavenProject/target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.softq.MavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
