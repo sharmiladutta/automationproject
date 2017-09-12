88503da08769eabd9bc382b3a6c885d482df26b7
sharmiladutta <sharmilatech2016@gmail.com>
2017-09-12 11:36:21 +0530

    Newprojectautomation
    Newprojectautomation

---
+++ Automation/src/main/java/com/icici/Automation/App.java
@@ -0,0 +1,13 @@
+package com.icici.Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
